7bddebbef0397aacd3e0c50408aaa6d50c405343
Farruxbek <nammtidev@gmail.com>
2022-12-21 16:25:03 +0500

    Navbar ni githab ga qoshish

---
+++ amaliyot/script.js
@@ -0,0 +1,11 @@
+
+
+let rgericon=document.querySelector('.brgericon');
+
+let navbars=document.querySelector('.navbar');
+
+
+rgericon.addEventListener("click", ()=>{
+    navbars.classList.toggle('navbar')
+})
+
---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+let navicon =document.querySelector('.nav_icon');
+let navbar=document.querySelector('.navbar');
+
+
+navicon.addEventListener('click', () =>{
+    navbar.classList.toggle('navbar') 
+})
+
+
+    
